61019ee21c7788a8caf0dbf0a2028b7dac624d16
karan yadav <xpresskaran@gmail.com>
2023-02-27 19:51:01 +0530

    created 3-option in saving and removed all dropdown onclick and created shortcut window

--- js/output.js
+++ js/output.js
@@ -1,39 +1,39 @@
 // Show output in iframe
 
 function getHtmlCode() {
-    return htmlCode.value;
+  return htmlCode.value;
 }
 
 function getCssCode() {
-    return cssCode.value;
+  return cssCode.value;
 }
 
 function getJsCode() {
-    return jsCode.value;
+  return jsCode.value;
 }
 
 function setIframeBody() {
-    let head = `
+  let head = `
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>${getCssCode()}</style>
     `;
-    showOutPutFrame.contentDocument.head.innerHTML = head;
-    let body = `${getHtmlCode()}` + `<script>${getJsCode()}</script>`;
-    showOutPutFrame.contentDocument.body.innerHTML = body;
-    showOutPutFrame.contentWindow.eval(getJsCode());
+  showOutPutFrame.contentDocument.head.innerHTML = head;
+  let body = `${getHtmlCode()}` + `<script>${getJsCode()}</script>`;
+  showOutPutFrame.contentDocument.body.innerHTML = body;
+  showOutPutFrame.contentWindow.eval(getJsCode());
 }
 
 htmlCode.addEventListener("keyup", () => {
-    setIframeBody();
-})
+  setIframeBody();
+});
 
 cssCode.addEventListener("keyup", () => {
-    setIframeBody();
-})
+  setIframeBody();
+});
 
 jsCode.addEventListener("keyup", () => {
-    setIframeBody();
-})
+  setIframeBody();
+});
